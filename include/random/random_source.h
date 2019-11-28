ed410e439a5957cc4dac9379b5a4dc5c112aaeb8
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-11-28 12:44:19 +0800

    internal include now merges to outside, the developer must adjust themselves to hide the internals

--- include/random/random_source.h
+++ include/random/random_source.h
@@ -16,6 +16,9 @@ extern "C" {
  * Get random resource from external DRBGs/NRBGs/physical sources
  *  (in our implementation we use system call to get entropy,
  *  so that we need to use fixed length and ignore the prediction resistance flag)
+ *  Be advised that the syscall limit the length the user get at once, you need to
+ *  read the system doc about this. (For MacOS use getentropy, for Linux use syscall
+ *  with flag SYS_getrandom)
  *
  * @param length bytes of result
  * @param result
