7465c476a7dc412d3f017150ac39cd13ad1d3ec3
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-12-10 10:21:58 +0800

    add example for wrp infrastructure

---
+++ include/wrp_items.h
@@ -0,0 +1,26 @@
+//
+// Created by Ghost on 2019/10/14.
+//
+
+#ifndef CRYPTO_FRAMEWORK_WRP_ITEMS_H
+#define CRYPTO_FRAMEWORK_WRP_ITEMS_H
+
+#include "cryptoconf.h"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+// WRP algorithms implementation here
+// for example
+//LIB_API const WRP_KEY *WRP_KEY_aes(void);
+//LIB_API const WRP_CIPHER *WRP_aes_128_ecb(void);
+//LIB_API const WRP_CIPHER *WRP_aes_128_cbc(void);
+//LIB_API const WRP_CIPHER *WRP_aes_128_ctr(void);
+//LIB_API const WRP_CIPHER *WRP_aes_128_gcm(void);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif //CRYPTO_FRAMEWORK_WRP_ITEMS_H
